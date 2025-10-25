6082fa63e5a630a0935a4a9f04567a512acfbb3d
Shreyas S <135410359+Shreyas-135@users.noreply.github.com>
2025-10-25 11:03:01 +0530

    bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { FileUpload } from './components/FileUpload';
 import { Dashboard } from './components/Dashboard';
-import { CompetitionInfo } from './components/CompetitionInfo';
 import { parseUploadedFiles } from './utils/parseVulnerabilities';
 import { ScanReport, UploadedFile } from './types/vulnerability';
 import { Shield } from 'lucide-react';
